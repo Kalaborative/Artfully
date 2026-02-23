2825eb4726e47d3c46cd2225c91100fe22a520b5
Kalaborative <kalaborative94@gmail.com>
2026-02-23 14:55:07 -0600

    fix drawing page build 3

--- client/src/components/profile/DrawingGallery.tsx
+++ client/src/components/profile/DrawingGallery.tsx
@@ -4,7 +4,6 @@ import type { SavedDrawing, Stroke, FillAction } from '@artfully/shared';
 import { MAX_SAVED_DRAWINGS } from '@artfully/shared';
 import Card from '../ui/Card';
 import ReplayOverlay from '../canvas/ReplayOverlay';
-import { storage, BUCKETS } from '../../lib/appwrite';
 
 interface DrawingGalleryProps {
   drawings: SavedDrawing[];
